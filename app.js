84df0074217550fcda2b5c34b92e1629986969db
Unknown <kamil.dmytrenko@gmail.com>
2018-03-12 19:44:47 +0100

    v4 commit

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
-const express    = require('express'),
-      bodyParser = require('body-parser'),
-      mongoose   = require('mongoose'),
-      path       = require('path'),
-      engine     = require('ejs-layout'),
-      Campground = require("./models/campground"),
-      Comment    = require('./models/comment'),
-      seedDB     = require("./seed"),
-      app        = express();
+const express    = require('express');
+const bodyParser = require('body-parser');
+const mongoose   = require('mongoose');
+const path       = require('path');
+const engine     = require('ejs-layout');
+const passport   = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const seedDB     = require("./seed");
+
+//MODELS
+const Campground = require("./models/campground");
+const Comment    = require('./models/comment');
+const User       = require('./models/user');
+
+const app        = express();
 
 seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs', engine.__express);
 
-// Campground.create({
-//     name: 'Mountain Goat\'s Rest',
-//     image: 'https://images.unsplash.com/photo-1484960055659-a39d25adcb3c?ixlib=rb-0.3.5&s=14948b1b6a8dd54164a0db522662e869&auto=format&fit=crop&w=1350&q=80',
-//     description: 'Nunquam talem heuretes.Azureus, mirabilis plasmators foris contactus de magnum, audax equiso.Ionicis tormentos ire in vasa!'
-// }, function(err, campground){
-//     if (err) {
-//         console.log("Error : " + err);
-//     } else {
-//         console.log("Created : " + campground);
-//     }
-// });
+//PASSPORT CONGIGURATION
+app.use(require('express-session')({
+    secret: "i dont know how to cook",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 //ROUTES
 
@@ -119,6 +127,8 @@ app.post('/campgrounds/:id/comments', function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-   console.log('Yelp Camp App running on port 3000');
-});
+//=====================================
+//AUTH ROUTES
+//=====================================
+
+module.exports = app;

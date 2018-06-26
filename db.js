f0857e826c1fc34e2e656eb1876731888f0617e7
Kamil <kamil.dmytrenko@gmail.com>
2018-06-26 19:25:03 +0200

    add db config

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+let port = process.env.PORT || 3000;
+let CONNECT_URI = 'mongodb://localhost/yelp-camp';
+// let CONNECT_URI = 'mongodb://admin:admin@ds121299.mlab.com:21299/movie-rest-app';
+
+module.exports = (app) => {
+  // Connect to database
+  mongoose.connect(CONNECT_URI)
+    .then(() => {
+      //Start app only after connection is ready
+      app.listen(port, () => {
+        console.log(`Your application is running on http://localhost:${port}`);
+      });
+    })
+    .catch(err => app.send(err));
+};

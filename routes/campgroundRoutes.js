1394811e9cc63d054e6b1c085712531765375c9e
Kamil <kamil.dmytrenko@gmail.com>
2018-06-27 17:16:27 +0200

    refactor routes

--- routes/campgroundRoutes.js
+++ routes/campgroundRoutes.js
@@ -1,16 +1,20 @@
 const router = require("express").Router({mergeParams: true});
-const middleware = require("../middleware");
 const controller = require('../controllers/campController');
+const multerService = require('../service/multerService');
+const middleware = require("../middleware");
+
+
 
 router.route('/')
   .get(controller.getAll)
-  .post(middleware.isLoggedIn, controller.addOne);
+  .post(middleware.isLoggedIn, multerService.upload.any('image'),controller.addOne);
 
 router.route('/new')
   .get(middleware.isLoggedIn, controller.getCreateForm);
 
-router.route('/id')
+router.route('/:id')
   .get(controller.showOne)
+  .delete(controller.deleteOne)
   .put(controller.updateOne);
 
 router.route("/:id/edit")

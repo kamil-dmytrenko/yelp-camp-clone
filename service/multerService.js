c0992835555efc6a7aac37fe83162c82b5cd47d5
Kamil <kamil.dmytrenko@gmail.com>
2018-06-27 17:15:58 +0200

    add multerService

---
+++ service/multerService.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+
+let storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+let imageFilter = function (req, file, cb) {
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
+};
+exports.upload = multer({ storage: storage, fileFilter: imageFilter});
+
+module.exports = exports;

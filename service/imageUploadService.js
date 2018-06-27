e293e2f15664e339eee274ce4a653c30f7c210d2
Kamil <kamil.dmytrenko@gmail.com>
2018-06-27 17:11:29 +0200

    add cloudify image upload service

---
+++ service/imageUploadService.js
@@ -0,0 +1,25 @@
+const cloudinary = require('cloudinary');
+const db = require("../models");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
+
+exports.upload = (req, res, campground) => {
+  req.files.forEach(async (file) => {
+    await cloudinary.uploader.upload(file.path)
+      .then(result => {
+        campground.images.push(result.secure_url);
+        if (req.files.length === campground.images.length) {
+          // Create a new campground and save to DB
+          db.Campground.create(campground)
+            .then(() => res.redirect("/campgrounds"))
+            .catch(err => console.log(err))
+        }
+      })
+  })
+};
+
+module.exports = exports;
